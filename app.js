9442c80e16b5d5e2dad3aa1b2059702bd18f39a4
aojpauga@gmail.com <aojpauga@gmail.com>
2019-01-25 11:50:20 -0800

    first commit

--- app.js
+++ app.js
@@ -1,133 +1,167 @@
-var characters = [
-  {
-    "name": "Link",
-    "image": "link.png"
-  },
-  {
-    "name": "Vivi",
-    "image": "vivismall.jpg"
-  },
-  {
-    "name": "Thief",
-    "image": "rogue.gif"
-  }
+var data = [{
+        "name": "Link",
+        "image": "link.png"
+    },
+    {
+        "name": "Vivi",
+        "image": "vivismall.jpg"
+    },
+    {
+        "name": "Thief",
+        "image": "rogue.gif"
+    }
 ]
 
-var addMage = function () {
-  var heading = document.querySelector("h1");
-  console.log("the heading is", heading);
-  heading.innerHTML = "You chose Mage!";
+var names = null; // list of objects
 
-  // change the heading again when the button is clicked
-  heading.innerHTML = "Mage";
-  heading.style.color = "#00FF00";
-  heading.style.backgroundColor = "blue";
+var addMage = function() {
+    var heading = document.querySelector("h1");
+    console.log("the heading is", heading);
 
-  // add an item to the list when the button is clicked
-  var theList = document.querySelector("#the-list");
-  console.log("the list is", theList);
-  var newItem = document.createElement("li");
-  var mageName = document.querySelector("#name");
-  newItem.innerHTML = "This is your Mage, " + mageName.value;
-  theList.appendChild(newItem);
+    // change the heading again when the button is clicked
+    heading.innerHTML = "Mage";
+    heading.style.color = "#00FF00";
+    heading.style.backgroundColor = "blue";
 
-  //add picture
-  var pic = document.createElement("IMG");
-  pic.setAttribute("src", "vivismall.jpg");
-  document.body.appendChild(pic);
+    // add an item to the list when the button is clicked
+    var titleDiv = document.querySelector("#title");
+    var mageName = document.querySelector("#name");
+    titleDiv.innerHTML = "This is your mage " + mageName.value;
+
+    //add picture
+    var imgDiv = document.querySelector("#image");
+    imgDiv.innerHTML = "";
+    var pic = document.createElement("img");
+    pic.setAttribute("src", "vivimage.jpg");
+    imgDiv.appendChild(pic);
 };
 
-var addWarrior = function () {
-  var heading = document.querySelector("h1");
-  console.log("the heading is", heading);
-  heading.innerHTML = "You chose Warrior!";
+var addWarrior = function() {
+    // addHero({
+    //   name: "Warrior",
+    //   image: "warriordnd.png",
+    //   color: "#00FF00",
+    //   backgroundColor: "red"
+    // });
 
-  // change the heading again when the button is clicked
-  heading.innerHTML = "Warrior";
-  heading.style.color = "#00FF00";
-  heading.style.backgroundColor = "red";
 
-  // add an item to the list when the button is clicked
-  var theList = document.querySelector("#the-list");
-  console.log("the list is", theList);
-  var newItem = document.createElement("li");
-  var warriorName = document.querySelector("#name");
-  newItem.innerHTML = "This is your Warrior, " + warriorName.value;
-  theList.appendChild(newItem);
 
-  //add picture
-  var pic = document.createElement("IMG");
-  pic.setAttribute("src", "link.png");
-  document.body.appendChild(pic);
+
+    var heading = document.querySelector("h1");
+    console.log("the heading is", heading);
+
+    // change the heading again when the button is clicked
+    heading.innerHTML = "Warrior";
+    heading.style.color = "#00FF00";
+    heading.style.backgroundColor = "red";
+
+    // add an item to the list when the button is clicked
+    var titleDiv = document.querySelector("#title");
+    var warriorName = document.querySelector("#name");
+    titleDiv.innerHTML = "This is your Warrior " + warriorName.value;
+
+    //add picture
+    var imgDiv = document.querySelector("#image");
+    imgDiv.innerHTML = "";
+    var pic = document.createElement("img");
+    pic.setAttribute("src", "warriordnd.png");
+    imgDiv.appendChild(pic);
 };
 
-var addRogue = function () {
-  var heading = document.querySelector("h1");
-  console.log("the heading is", heading);
-  heading.innerHTML = "You chose Rogue!";
+var addRogue = function() {
+    var heading = document.querySelector("h1");
+    console.log("the heading is", heading);
 
-  // change the heading again when the button is clicked
-  heading.innerHTML = "Rogue";
-  heading.style.color = "#00FF00";
-  heading.style.backgroundColor = "green";
+    // change the heading again when the button is clicked
+    heading.innerHTML = "Rogue";
+    heading.style.color = "#00FF00";
+    heading.style.backgroundColor = "green";
 
-  // add an item to the list when the button is clicked
-  var theList = document.querySelector("#the-list");
-  console.log("the list is", theList);
-  var newItem = document.createElement("li");
-  var rogueName = document.querySelector("#name");
-  newItem.innerHTML = "This is your rogue, " + rogueName.value;
-  theList.appendChild(newItem);
+    // add an item to the list when the button is clicked
+    var titleDiv = document.querySelector("#title");
+    var rogueName = document.querySelector("#name");
+    titleDiv.innerHTML = "This is your rogue " + rogueName.value;
 
-  //add picture
-  var pic = document.createElement("IMG");
-  pic.setAttribute("src", "rogue.gif");
-  document.body.appendChild(pic);
+    //add picture
+    var imgDiv = document.querySelector("#image");
+    imgDiv.innerHTML = "";
+    var pic = document.createElement("img");
+    pic.setAttribute("src", "rogue.gif");
+    imgDiv.appendChild(pic);
 };
 
-var addRandomPlayer = function (){
-  var r = Math.random();
-  if (r < 0.3333) {
-    addWarrior();
-  } else if (r < 0.6666) {
-    addMage();
-  } else {
-    addRogue();
-  }
+var addRandomPlayer = function() {
+    var r = Math.random();
+    if (r < 0.3333) {
+        addWarrior();
+    } else if (r < 0.6666) {
+        addMage();
+    } else {
+        addRogue();
+    }
 };
 
 var warriorButton = document.querySelector("#warrior-button");
 console.log("the button is", warriorButton);
-warriorButton.onclick = function () {
-  addWarrior();
+warriorButton.onclick = function() {
+    addWarrior();
 }
 var mageButton = document.querySelector("#mage-button");
 console.log("the button is", mageButton);
-mageButton.onclick = function () {
-  addMage();
+mageButton.onclick = function() {
+    addMage();
 }
 var rogueButton = document.querySelector("#rogue-button");
 console.log("the button is", rogueButton);
-rogueButton.onclick = function () {
-  addRogue();  
+rogueButton.onclick = function() {
+    addRogue();
 }
 
 var randButtom = document.querySelector('#random-button');
 console.log("the button is", randButtom);
 randButtom.onclick = function() {
-  addRandomPlayer();
+    addRandomPlayer();
 }
 
-fetch("https://api.myjson.com/bins/pmt6k").then(function (response) {
-  response.json().then(function (data) {
-    console.log("data loaded from server", data);
-    data.forEach(function(ch) {
-      var img = document.createElement("img");
-      img.src = ch.image;
-      document.appendChild(img);
+var addHero = function(character) {
+    var heading = document.querySelector("h1");
+    console.log("the heading is", heading);
+
+    // change the heading again when the button is clicked
+    heading.innerHTML = character.name;
+    heading.style.color = "#00FF00";
+    heading.style.backgroundColor = "red";
+
+    // add an item to the list when the button is clicked
+    var titleDiv = document.querySelector("#title");
+    titleDiv.innerHTML = "This is " + character.name;
+
+    //add picture
+    var imgDiv = document.querySelector("#image");
+    imgDiv.innerHTML = "";
+    var pic = document.createElement("img");
+    pic.setAttribute("src", character.image);
+    imgDiv.appendChild(pic);
+};
+
+fetch("https://api.myjson.com/bins/pmt6k").then(function(response) {
+    response.json().then(function(data) {
+        console.log("data loaded from server", data);
+        names = data; // save for later
+        // append to the page:
+        var buttonsList = document.querySelector("#buttons")
+        data.forEach(function(ch) {
+            var b = document.createElement("button");
+            b.innerHTML = `My Hero: ${ch.name}`;
+            buttonsList.appendChild(b);
+            b.onclick = function() {
+                addHero(ch);
+            };
+        });
     });
-  });
 });
+
+
 
 
 //adding random - Math.random() - to convert to int - Math.floor(Math.random());
@@ -135,5 +169,3 @@ fetch("https://api.myjson.com/bins/pmt6k").then(function (response) {
 //ajax - asychronous java script xlm
 //fetch api -> fetch("paste url").then(function(response){respons.json().then(function(data)){console.log("the," data);}););
 //document.querrySelectorAll()
-
-
